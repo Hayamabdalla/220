95f74daace49a868cb889eeba8162856793a23e1
hayam <abdalla.hayam@yahoo.com>
2021-09-03 21:37:58 -0400

    I just submitting it because of the due date, but it is failing with me, and I don't know what to do all I can think of is try to do more than one and submit them but for sure I think the mistake is because  of my pycharm. thank you!

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,22 @@
+"""
+name: Hayam Abdalla
+hello.py
+
+problem: this program is to def main function
+
+certification of authenticity:
+i certify that this assignment is entirely my own work.
+"""
+
+def main():
+
+    print('hello,world!')
+
+main()
+
+
+
+
+
+
+
