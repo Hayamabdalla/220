7fec1b1457cc7c82ece6c8c910f1c675e0e774e8
hayam <abdalla.hayam@yahoo.com>
2021-09-03 21:37:58 -0400

    I just submitting it because of the due date, but it is failing with me, and I don't know what to do all I can think of is try to do more than one and submit them but for sure I think the mistake is because  of my pycharm. thank you! I actually tried again and the first time I run the test it gives me that I passed but when I repeated to make sure it gives me ignored I don't know what is wrong.

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,15 @@
+"""
+name: Hayam Abdalla
+hello.py
+
+problem: print hello world
+certification of authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+def main():
+    print("hello, world!")
+
+main()
+
+
